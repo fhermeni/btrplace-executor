f31366dc0168b33b203dc5f9e47607cb269f6885
Fabien Hermenier <hermenierfabien@gmail.com>
2014-04-15 12:48:12 +0200

    bug fix

--- snapshot_javadoc.sh
+++ snapshot_javadoc.sh
@@ -11,11 +11,8 @@ WWW=$1
 
 echo "Publishing javadoc into ${WWW}"
 mkdir -p ${WWW}
-
-#sub-modules apidoc
-echo "Generate the javadoc for the modules"
 mvn javadoc:javadoc > /dev/null
 mvn javadoc:jar > /dev/null
 mkdir -p ${WWW}/${ARTIFACT_ID} > /dev/null
 rm -rf ${WWW}/${ARTIFACT_ID}/apidocs-snapshot
-mv ${ARTIFACT_ID}/target/apidocs ${WWW}/${ARTIFACT_ID}/apidocs-snapshot
+mv target/apidocs ${WWW}/${ARTIFACT_ID}/apidocs-snapshot
